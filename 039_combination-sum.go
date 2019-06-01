898da93f732e595aa8420cb07914f5960d608684
tkachenko <olexiy.tkachenko@gmail.com>
2019-06-01 17:37:13 +0300

    040

--- 039_combination-sum.go
+++ 039_combination-sum.go
@@ -24,3 +24,24 @@ func calcCombinationSum(result *[][]int, current []int, target int, candidates [
 		}
 	}
 }
+
+func calcCombinationSum2(result *[][]int, current []int, target int, candidates []int) {
+	vPrev := -1
+	for i, v := range candidates {
+		if v == vPrev {
+			continue
+		}
+		vPrev = v
+		switch {
+		case v < target:
+			calcCombinationSum(result, append(current, v), target-v, candidates[i+1:])
+		case v == target:
+			r := make([]int, len(current)+1)
+			copy(r, current)
+			r[len(current)] = v
+			*result = append(*result, r)
+		default:
+			break
+		}
+	}
+}
