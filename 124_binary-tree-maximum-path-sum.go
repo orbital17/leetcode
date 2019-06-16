b41a4776d1422b2cb28a32f43c95fd039983090c
tkachenko <olexiy.tkachenko@gmail.com>
2019-06-16 20:41:35 +0300

    124

---
+++ 124_binary-tree-maximum-path-sum.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import . "./lib"
+
+func maxPathSum(root *TreeNode) int {
+	result := root.Val
+	maxPathSumHelper(root, &result)
+	return result
+}
+
+func maxPathSumHelper(node *TreeNode, result *int) int {
+	if node == nil {
+		return 0
+	}
+	l := maxPathSumHelper(node.Left, result)
+	r := maxPathSumHelper(node.Right, result)
+	if l+r+node.Val > *result {
+		*result = l + r + node.Val
+	}
+	return Max(node.Val+Max(l, r), 0)
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
