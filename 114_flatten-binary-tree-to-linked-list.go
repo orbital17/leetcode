d548d23900a73f18302b1053b6f95787234e7be9
tkachenko <olexiy.tkachenko@gmail.com>
2019-06-16 19:52:32 +0300

    114

---
+++ 114_flatten-binary-tree-to-linked-list.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import . "./lib"
+
+func flatten(root *TreeNode) {
+	var cur, a *TreeNode
+	for cur = root; cur != nil; cur = cur.Right {
+		if cur.Left != nil {
+			a = cur.Left
+			for a.Right != nil {
+				a = a.Right
+			}
+			a.Right = cur.Right
+			cur.Right = cur.Left
+			cur.Left = nil
+		}
+	}
+}
