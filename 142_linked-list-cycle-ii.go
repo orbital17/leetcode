a10f623530ec1ddb9ff82cc17d0fc2c87505e3ee
tkachenko <olexiy.tkachenko@gmail.com>
2019-06-18 20:06:22 +0300

    142

---
+++ 142_linked-list-cycle-ii.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import . "./lib"
+
+func detectCycle(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for {
+		if fast == nil || fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			break
+		}
+	}
+	slow = head
+	for fast != slow {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	return fast
+}
