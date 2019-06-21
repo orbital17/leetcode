027e3e0d79e95a4ba516f2de6b94b248c66c3903
tkachenko <olexiy.tkachenko@gmail.com>
2019-06-21 16:35:42 +0300

    169 update

--- 169_majority-element.go
+++ 169_majority-element.go
@@ -4,17 +4,14 @@ package leetcode
 
 func majorityElement(nums []int) int {
 	var i, count, cur int
-	for i < len(nums) {
-		cur = nums[i]
-		count = 1
-		i++
-		for count != 0 && i < len(nums) {
-			if cur == nums[i] {
-				count++
-			} else {
-				count--
-			}
-			i++
+	for i = 0; i < len(nums); i++ {
+		if count == 0 {
+			count++
+			cur = nums[i]
+		} else if nums[i] == cur {
+			count++
+		} else {
+			count--
 		}
 	}
 	return cur
