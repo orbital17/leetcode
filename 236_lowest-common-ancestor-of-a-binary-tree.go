54e006fa57962600d5bd2996849d496e12d449fa
tkachenko <olexiy.tkachenko@gmail.com>
2019-07-02 18:19:20 +0300

    236 iterative

--- 236_lowest-common-ancestor-of-a-binary-tree.go
+++ 236_lowest-common-ancestor-of-a-binary-tree.go
@@ -2,21 +2,57 @@ package leetcode
 
 import . "./lib"
 
-// import "container/list"
+import "container/list"
 
-/**
- * Definition for TreeNode.
- * type TreeNode struct {
- *     Val int
- *     Left *ListNode
- *     Right *ListNode
- * }
- */
+type LCAState int
+
+const (
+	BP LCAState = iota
+	LD
+	BD
+)
+
+type LCAItem struct {
+	node  *TreeNode
+	state LCAState
+}
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	var ans *TreeNode
-	lowestCommonAncestorHelper(root, p, q, &ans)
-	return ans
+
+	stack := list.New()
+	stack.PushBack(&LCAItem{root, BP})
+
+	oneFound := false
+	var lcaIndex int
+	var lca *LCAItem
+
+	for stack.Len() != 0 {
+		e := stack.Back().Value.(*LCAItem)
+		switch {
+		case e.node == nil || e.state == BD:
+			stack.Remove(stack.Back())
+			if oneFound && stack.Len() == lcaIndex {
+				lcaIndex--
+				lca = stack.Back().Value.(*LCAItem)
+			}
+		case e.state == BP:
+			if e.node == p || e.node == q {
+				if oneFound {
+					return lca.node
+				} else {
+					oneFound = true
+					lcaIndex = stack.Len() - 1
+					lca = e
+				}
+			}
+			stack.PushBack(&LCAItem{e.node.Left, BP})
+			e.state = LD
+		case e.state == LD:
+			stack.PushBack(&LCAItem{e.node.Right, BP})
+			e.state = BD
+		}
+	}
+	panic("")
 }
 
 func lowestCommonAncestorRec(root, p, q *TreeNode) *TreeNode {
