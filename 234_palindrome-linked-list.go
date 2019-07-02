6c1e920f52fd8ebba05545adc7bf579e1b0a9307
tkachenko <olexiy.tkachenko@gmail.com>
2019-07-02 13:49:42 +0300

    234

---
+++ 234_palindrome-linked-list.go
@@ -0,0 +1,44 @@
+package leetcode
+
+import . "./lib"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+	cur := head
+	n := 0
+	for cur != nil {
+		n++
+		cur = cur.Next
+	}
+	m := (n + 1) / 2
+	cur = head
+	for i := 0; i < m; i++ {
+		cur = cur.Next
+	}
+	var prev *ListNode
+	for cur != nil {
+		n := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = n
+	}
+	a := prev
+	b := head
+	for a != nil {
+		if a.Val != b.Val {
+			return false
+		}
+		a = a.Next
+		b = b.Next
+	}
+	return true
+}
