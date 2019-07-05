f1e6191cb48fa79ee20d4818e8a8a30976504bf7
tkachenko <olexiy.tkachenko@gmail.com>
2019-07-05 12:34:42 +0300

    refactor code

--- lib/solved.go
+++ lib/solved.go
@@ -1,7 +1,9 @@
 package lib
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func calcSolved() int {
@@ -15,4 +17,31 @@ func calcSolved() int {
 		dict[k] = true
 	}
 	return len(dict)
+}
+
+func Refactor() error {
+	files, _ := ioutil.ReadDir(".")
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		err := moveToFolder(file.Name())
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func moveToFolder(filename string) error {
+	f := filename[:len(filename)-3]
+	if f[len(f)-5:] == "_test" {
+		f = f[:len(f)-5]
+	}
+	err := os.MkdirAll(f, 0755)
+	if err != nil {
+		return err
+	}
+	err = os.Rename(filename, fmt.Sprintf("%s/%s", f, filename))
+	return err
 }
---
+++ main_test.go
@@ -0,0 +1,13 @@
+package leetcode
+
+import (
+	"fmt"
+)
+
+import "./lib"
+
+func Example_refactor() {
+	fmt.Print(lib.Refactor())
+	//Output:
+	//
+}
